b6e31543ca71e7b8b0e5cc9af5db82465ac12825
Rizwan Memon <rizwan2000.rm@gmail.com>
2024-12-04 00:13:51 +0530

    day 03: part 1

---
+++ src/day3/part1.rs
@@ -0,0 +1,26 @@
+extern crate regex;
+
+use regex::Regex;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn main() -> io::Result<()> {
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut uncorrupted_total = 0;
+
+    for line in reader.lines() {
+        let line_content = line?;
+        for cap in regex.captures_iter(&line_content) {
+            let num1 = cap.get(1).unwrap().as_str();
+            let num2 = cap.get(2).unwrap().as_str();
+            uncorrupted_total += (num1.parse::<i32>().unwrap()) * (num2.parse::<i32>().unwrap());
+        }
+    }
+
+    println!("{}", uncorrupted_total);
+
+    Ok(())
+}
